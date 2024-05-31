8e7662d04e0bcb17d9ee872d70d33e426420cb68
amir-roshan <amirvalielahi@gmail.com>
2024-05-30 20:39:49 -0700

    Init Fitness Exercises App

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,22 +1,23 @@
+/* eslint-disable indent */
 export const exerciseOptions = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-  },
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    },
 };
 
 export const youtubeOptions = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
-  },
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+        'X-RapidAPI-Key': '6b18472347mshd49a8e223840137p1da68ejsnda8a8698e93f',
+    },
 };
 
 export const fetchData = async (url, options) => {
-  const res = await fetch(url, options);
-  const data = await res.json();
+    const res = await fetch(url, options);
+    const data = await res.json();
 
-  return data;
+    return data;
 };
